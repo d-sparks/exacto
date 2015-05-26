f074e1e7186bbef8e03d85df1f374b911db2270a
Dan Sparks <dan.sparks@clever.com>
2015-05-26 09:13:58 -0700

    Add sript for divide comparison to an XBoard engine

---
+++ util/divide.go
@@ -0,0 +1,156 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"os/exec"
+	"strconv"
+	"strings"
+)
+
+type process struct {
+	Stdin             chan string
+	Stdout            chan string
+	UnderlyingProcess *exec.Cmd
+}
+
+func panicIfErr(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func NewProcess(command string) process {
+	underlyingProcess := exec.Command(command)
+	stdinPipe, err := underlyingProcess.StdinPipe()
+	panicIfErr(err)
+	stdoutPipe, err := underlyingProcess.StdoutPipe()
+	panicIfErr(err)
+
+	stdinWriter := bufio.NewWriter(stdinPipe)
+	stdinChan := make(chan string)
+	go func() {
+		for s := range stdinChan {
+			fmt.Println("Writing ", s)
+			_, err := stdinWriter.WriteString(s + "\n")
+			panicIfErr(err)
+			stdinWriter.Flush()
+		}
+	}()
+
+	stdoutReader := bufio.NewReader(stdoutPipe)
+	stdoutChan := make(chan string)
+	go func() {
+		for {
+			line, err := stdoutReader.ReadString('\n')
+			fmt.Println("Reading ", line)
+			panicIfErr(err)
+			stdoutChan <- line
+		}
+	}()
+
+	go underlyingProcess.Run()
+
+	return process{
+		Stdin:             stdinChan,
+		Stdout:            stdoutChan,
+		UnderlyingProcess: underlyingProcess,
+	}
+}
+
+func printMany(c chan string) {
+	for s := range c {
+		fmt.Print(s)
+	}
+}
+
+func readDivide(c chan string) map[string]string {
+	out := map[string]string{}
+	var move, count string
+	for move != "moves" {
+		line := <-c
+		split := strings.Split(line, " ")
+		if len(split) >= 2 {
+			move, count = split[0], split[1]
+			out[move] = count
+		}
+	}
+	return out
+}
+
+// returns "DIFFER" if the moves in this position are differen't, and "SAME" if the totals match.
+func getDifferingMove(depth int, p1, p2 process) string {
+	depthString := strconv.Itoa(depth)
+	p1.Stdin <- "divide " + depthString
+	p2.Stdin <- "divide " + depthString
+
+	results1 := readDivide(p1.Stdout)
+	results2 := readDivide(p2.Stdout)
+
+	if results1["total"] == results2["total"] {
+		return "SAME"
+	}
+	delete(results1, "total")
+	delete(results2, "total")
+
+	if results1["moves"] != results2["moves"] {
+		return "DIFFER"
+	}
+
+	for k, v := range results1 {
+		v2 := results2[k]
+		if v != v2 {
+			return k
+		}
+	}
+
+	for k, v := range results2 {
+		v2 := results1[k]
+		if v != v2 {
+			return k
+		}
+	}
+
+	return "WTF"
+}
+
+func getDifferingMoveSequence(depth int, p1, p2 process) string {
+	difference := getDifferingMove(depth, p1, p2)
+	if difference == "SAME" {
+		return "SAME"
+	}
+	if difference == "DIFFER" {
+		return "DIFFER"
+	}
+	fmt.Println("DD: ", depth, difference)
+
+	p1.Stdin <- "usermove " + difference
+	p2.Stdin <- "usermove " + difference
+	fmt.Println(difference)
+	return difference + " " + getDifferingMoveSequence(depth-1, p1, p2)
+}
+
+func main() {
+	engine1 := flag.String("engine1", "", "path to engine1 executable")
+	engine2 := flag.String("engine2", "", "path to engine2 executable")
+	// fenFile := flag.String("fen", "", "path to FEN file")
+	// depth := flag.Int("depth", 4, "depth to search each position")
+	flag.Parse()
+
+	process1 := NewProcess(*engine1)
+	process2 := NewProcess(*engine2)
+	for i := 0; i < 7; i++ {
+		<-process1.Stdout
+		<-process2.Stdout
+	}
+	<-process2.Stdout
+	<-process2.Stdout
+
+	process1.Stdin <- "force"
+	process2.Stdin <- "force"
+
+	// set boards if necessary
+
+	fmt.Println(getDifferingMoveSequence(4, process1, process2))
+}
