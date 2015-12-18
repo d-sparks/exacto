6e6d35b29da69ccbc27562d5d9cc5772f470a5fa
Dan Sparks <dan.sparks@clever.com>
2015-12-18 09:29:35 -0800

    Spruce up divide script

--- util/divide.go
+++ util/divide.go
@@ -10,6 +10,7 @@ import (
 )
 
 type process struct {
+	Label             string
 	Stdin             chan string
 	Stdout            chan string
 	UnderlyingProcess *exec.Cmd
@@ -21,7 +22,7 @@ func panicIfErr(err error) {
 	}
 }
 
-func NewProcess(command string) process {
+func NewProcess(command, label string) process {
 	underlyingProcess := exec.Command(command)
 	stdinPipe, err := underlyingProcess.StdinPipe()
 	panicIfErr(err)
@@ -32,7 +33,7 @@ func NewProcess(command string) process {
 	stdinChan := make(chan string)
 	go func() {
 		for s := range stdinChan {
-			fmt.Println("Writing ", s)
+			fmt.Printf("send %s to %s\n", s, label)
 			_, err := stdinWriter.WriteString(s + "\n")
 			panicIfErr(err)
 			stdinWriter.Flush()
@@ -44,7 +45,7 @@ func NewProcess(command string) process {
 	go func() {
 		for {
 			line, err := stdoutReader.ReadString('\n')
-			fmt.Println("Reading ", line)
+			fmt.Printf("%s: %s", label, line)
 			panicIfErr(err)
 			stdoutChan <- line
 		}
@@ -53,6 +54,7 @@ func NewProcess(command string) process {
 	go underlyingProcess.Run()
 
 	return process{
+		Label:             label,
 		Stdin:             stdinChan,
 		Stdout:            stdoutChan,
 		UnderlyingProcess: underlyingProcess,
@@ -123,7 +125,7 @@ func getDifferingMoveSequence(depth int, p1, p2 process) string {
 	if difference == "DIFFER" {
 		return "DIFFER"
 	}
-	fmt.Println("DD: ", depth, difference)
+	fmt.Printf("Depth %d, difference %s\n", depth, difference)
 
 	p1.Stdin <- "usermove " + difference
 	p2.Stdin <- "usermove " + difference
@@ -131,26 +133,37 @@ func getDifferingMoveSequence(depth int, p1, p2 process) string {
 	return difference + " " + getDifferingMoveSequence(depth-1, p1, p2)
 }
 
+func getEngineProcess(path, label string, headerLines int, commands ...string) process {
+	process := NewProcess(path, label)
+	for i := 0; i < headerLines; i++ {
+		<-process.Stdout
+	}
+	for _, command := range commands {
+		process.Stdin <- command
+	}
+	return process
+}
+
 func main() {
 	engine1 := flag.String("engine1", "", "path to engine1 executable")
 	engine2 := flag.String("engine2", "", "path to engine2 executable")
+	label1 := flag.String("label1", "engine1", "label for engine1 loglines")
+	label2 := flag.String("label2", "engine2", "label for engine2 loglines")
+	headers1 := flag.Int("headers1", 7, "number of header loglines to ignore (engine 1)")
+	headers2 := flag.Int("headers2", 7, "number of header loglines to ignore (engine 2)")
+	commands1 := flag.String("commands1", "force", "comma-separated init commands (engine 1)")
+	commands2 := flag.String("commands2", "force", "comma-separated init commands (engine 2)")
 	// fenFile := flag.String("fen", "", "path to FEN file")
 	// depth := flag.Int("depth", 4, "depth to search each position")
 	flag.Parse()
 
-	process1 := NewProcess(*engine1)
-	process2 := NewProcess(*engine2)
-	for i := 0; i < 7; i++ {
-		<-process1.Stdout
-		<-process2.Stdout
-	}
-	<-process2.Stdout
-	<-process2.Stdout
+	sliceCommands1 := strings.Split(*commands1, ",")
+	sliceCommands2 := strings.Split(*commands2, ",")
 
-	process1.Stdin <- "force"
-	process2.Stdin <- "force"
+	process1 := getEngineProcess(*engine1, *label1, *headers1, sliceCommands1...)
+	process2 := getEngineProcess(*engine2, *label2, *headers2, sliceCommands2...)
 
 	// set boards if necessary
 
-	fmt.Println(getDifferingMoveSequence(4, process1, process2))
+	fmt.Println(getDifferingMoveSequence(5, process1, process2))
 }
