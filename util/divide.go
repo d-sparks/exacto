bdbe97931df9c6044969d73210f2f9fb063ffc14
Dan Sparks <dan.sparks@clever.com>
2015-12-24 08:17:29 -0800

    More improvements to divide script

--- util/divide.go
+++ util/divide.go
@@ -61,28 +61,29 @@ func NewProcess(command, label string) process {
 	}
 }
 
-func printMany(c chan string) {
-	for s := range c {
-		fmt.Print(s)
-	}
+type divide struct {
+	Total  string
+	Moves  int
+	Output map[string]string
 }
 
-func readDivide(c chan string) map[string]string {
-	out := map[string]string{}
+func readDivide(c chan string) divide {
+	output := map[string]string{}
 	var move, count string
 	for move != "moves" {
 		line := <-c
 		split := strings.Split(line, " ")
 		if len(split) >= 2 {
 			move, count = split[0], split[1]
-			out[move] = count
+			output[move] = count
 		}
 	}
-	return out
+	total := output["total"]
+	delete(output, "total")
+	return divide{Total: total, Moves: len(output), Output: output}
 }
 
-// returns "DIFFER" if the moves in this position are differen't, and "SAME" if the totals match.
-func getDifferingMove(depth int, p1, p2 process) string {
+func getDifferingMoves(depth int, p1, p2 process) (plus, differ, minus []string) {
 	depthString := strconv.Itoa(depth)
 	p1.Stdin <- "divide " + depthString
 	p2.Stdin <- "divide " + depthString
@@ -90,53 +91,57 @@ func getDifferingMove(depth int, p1, p2 process) string {
 	results1 := readDivide(p1.Stdout)
 	results2 := readDivide(p2.Stdout)
 
-	if results1["total"] == results2["total"] {
-		return "SAME"
-	}
-	delete(results1, "total")
-	delete(results2, "total")
-
-	if results1["moves"] != results2["moves"] {
-		return "DIFFER"
-	}
-
-	for k, v := range results1 {
-		v2 := results2[k]
-		if v != v2 {
-			return k
+	for k, v := range results1.Output {
+		v2, ok := results2.Output[k]
+		if !ok {
+			plus = append(plus, k)
+		} else if v != v2 {
+			differ = append(differ, k)
 		}
 	}
 
-	for k, v := range results2 {
-		v2 := results1[k]
-		if v != v2 {
-			return k
+	for k, v := range results2.Output {
+		v1, ok := results1.Output[k]
+		if !ok {
+			minus = append(minus, k)
+		} else if v != v1 {
+			differ = append(differ, k)
 		}
 	}
 
-	return "WTF"
+	return
 }
 
 func getDifferingMoveSequence(depth int, p1, p2 process) string {
-	difference := getDifferingMove(depth, p1, p2)
-	if difference == "SAME" {
-		return "SAME"
+	plus, differ, minus := getDifferingMoves(depth, p1, p2)
+	output := []string{""}
+	if depth == 0 || len(plus) > 0 || len(minus) > 0 {
+		if len(plus) > 0 {
+			output = append(output, "Engine 1 had: "+strings.Join(plus, ", "))
+		}
+		if len(minus) > 0 {
+			output = append(output, "Engine 2 had: "+strings.Join(minus, ", "))
+		}
+		return strings.Join(output, "\n")
 	}
-	if difference == "DIFFER" {
-		return "DIFFER"
-	}
-	fmt.Printf("Depth %d, difference %s\n", depth, difference)
 
-	p1.Stdin <- "usermove " + difference
-	p2.Stdin <- "usermove " + difference
-	fmt.Println(difference)
-	return difference + " " + getDifferingMoveSequence(depth-1, p1, p2)
+	if len(differ) == 0 {
+		return ""
+	}
+
+	move := differ[0]
+
+	p1.Stdin <- "usermove " + move
+	p2.Stdin <- "usermove " + move
+	return move + " " + getDifferingMoveSequence(depth-1, p1, p2)
 }
 
 func getEngineProcess(path, label string, headerLines int, commands ...string) process {
 	process := NewProcess(path, label)
+	process.Stdin <- "force"
 	for i := 0; i < headerLines; i++ {
 		<-process.Stdout
+
 	}
 	for _, command := range commands {
 		process.Stdin <- command
@@ -150,7 +155,7 @@ func main() {
 	label1 := flag.String("label1", "engine1", "label for engine1 loglines")
 	label2 := flag.String("label2", "engine2", "label for engine2 loglines")
 	headers1 := flag.Int("headers1", 7, "number of header loglines to ignore (engine 1)")
-	headers2 := flag.Int("headers2", 9, "number of header loglines to ignore (engine 2)")
+	headers2 := flag.Int("headers2", 7, "number of header loglines to ignore (engine 2)")
 	commands1 := flag.String("commands1", "force", "comma-separated init commands (engine 1)")
 	commands2 := flag.String("commands2", "force", "comma-separated init commands (engine 2)")
 	// fenFile := flag.String("fen", "", "path to FEN file")
@@ -165,7 +170,7 @@ func main() {
 
 	// set boards if necessary
 
-	fmt.Println(getDifferingMoveSequence(3, process1, process2))
+	fmt.Println("\n" + getDifferingMoveSequence(4, process1, process2))
 
 	// exit
 	process1.Stdin <- "exit"
